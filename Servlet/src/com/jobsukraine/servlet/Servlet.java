e79fd84c96f89845159d1c7922dc26822749bdba
Denys Sikora <sikora.den@gmail.com>
2015-07-15 22:58:31 +0300

    5645566y

--- Servlet/src/com/jobsukraine/servlet/Servlet.java
+++ Servlet/src/com/jobsukraine/servlet/Servlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/Se1rvlet")
 public class Servlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	 private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
